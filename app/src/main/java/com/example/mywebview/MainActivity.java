80dcdd3be2afc099092cafb6831dc18552fca83f
David <davidmcelhinney10@hotmail.com>
2019-10-01 22:32:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mywebview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mywebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.loadUrl("https://www.ulster.ac.uk");
+    }
+}
